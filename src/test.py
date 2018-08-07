b85f2e8d083d69f54e57acdc838051f6bc2d67e7
Hemanth <tammareddy53@gmail.com>
2018-08-07 10:30:21 -0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -111,14 +111,11 @@ def Main(operation, args):
             print("Product Transferred to Shipper")
             return True
 
-    if operation == 'UpdateAssetInformation':
+    if operation == 'UpdateAssetInformations':
         #Shipping company like FedEx will update final status after product is delivered
         print('Product Shipped to customer')
         if is_owner(shipping_company_depot_hash,case_id):
-            status = "Delivered"
-            #date_time_delivered = datetime.datetime.now()
-            Put(GetContext(), case_id,ordered_by_name,orderedby_company_hash,shipping_company_depot_code,shipping_company_depot_hash,carrier_company_hash,product_id,quantity,customer_name,customer_address,delivery_by,status)
-            print("Product Delivered to customer")
+          
             return True
         
     if operation == 'GetStatusOfShipment':
